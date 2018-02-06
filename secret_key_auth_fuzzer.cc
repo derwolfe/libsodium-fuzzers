1b4315c40272aa6c7622959b666aa125c089c687
Chris Wolfe <chriswwolfe@gmail.com>
2018-02-06 09:35:45 -0600

    wip

--- secret_key_auth_fuzzer.cc
+++ secret_key_auth_fuzzer.cc
@@ -6,6 +6,19 @@
 
 #include "fake_random.h"
 
+int is_subarray(const unsigned char * haystack, const size_t haystack_size,
+                 const unsigned char * need, const size_t needle_size) {
+  if (haystack_size < needle_size) {
+    return -1;
+  }
+
+  // iterate over the haystack, memcmp-ing each time. What about buffer over reads?
+  size_t ct = 0;
+  for (ct; ct++, ct < haystack_size) {
+
+  }
+}
+
 extern "C" int LLVMFuzzerTestOneInput(const unsigned char *data, size_t size) {
   int initialized = sodium_init();
   assert(initialized >= 0);
@@ -31,10 +44,10 @@ extern "C" int LLVMFuzzerTestOneInput(const unsigned char *data, size_t size) {
   assert(check == 0);
 
   // none of the key material should be present in the mac
-  unsigned char *offset = std::search(std::begin(mac), std::end(mac),
-                                      std::begin(key), std::end(key));
-  bool found = offset == std::end(key);
-  assert(!found);
-
+  auto key_end = std::end(key);
+  // unsigned char *offset = std::search(std::begin(mac), std::end(mac),
+  //                                     std::begin(key), std::end(key));
+  // if (offset != std::end(key)) {
+  // }
   return 0;
 }
